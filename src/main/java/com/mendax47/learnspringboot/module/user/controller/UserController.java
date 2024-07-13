0315fee63f3fd29a51a7b1469d47dc1b23dc49e2
MRParag95 <rahman.parag.95@gmail.com>
2024-07-13 10:32:32 +0600

    Initial Project Setup.

--- src/main/java/com/mendax47/learnspringboot/module/user/controller/UserController.java
+++ src/main/java/com/mendax47/learnspringboot/module/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.mendax47.learnspringboot.module.user.controller;
 import com.mendax47.learnspringboot.constants.routers.UserRoutes;
 import com.mendax47.learnspringboot.generics.dtos.responses.GenericResponseDTO;
 import com.mendax47.learnspringboot.module.user.dtos.requests.UserRequestDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(UserRoutes.USER_ROUTE)
+@RequestMapping( UserRoutes.USER_ROUTE )
 public class UserController implements IUserController {
-    @PostMapping(UserRoutes.USER_REGISTRATION)
+    @PostMapping( UserRoutes.USER_REGISTRATION )
     @Override
-    public ResponseEntity<GenericResponseDTO> create(@Validated UserRequestDto request) {
+    public ResponseEntity< GenericResponseDTO > create( @Validated UserRequestDto request ) {
         return null;
     }
 }
