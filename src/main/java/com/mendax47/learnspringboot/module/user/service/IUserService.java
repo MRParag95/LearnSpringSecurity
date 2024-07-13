c077728305ebf697763c89a8c105f6aedecf58c8
MRParag95 <rahman.parag.95@gmail.com>
2024-07-13 15:51:08 +0600

    Initial Project Setup.

--- src/main/java/com/mendax47/learnspringboot/module/user/service/IUserService.java
+++ src/main/java/com/mendax47/learnspringboot/module/user/service/IUserService.java
@@ -1,19 +1,10 @@
 package com.mendax47.learnspringboot.module.user.service;
 
 import com.mendax47.learnspringboot.generics.dtos.responses.GenericResponseDTO;
-import com.mendax47.learnspringboot.generics.dtos.responses.PageDataResponseDTO;
 import com.mendax47.learnspringboot.generics.services.ICRUDService;
 import com.mendax47.learnspringboot.module.user.User;
 import com.mendax47.learnspringboot.module.user.dtos.requests.UserRequestDto;
 import com.mendax47.learnspringboot.module.user.dtos.responses.CustomUserResponseDTO;
 
 public interface IUserService extends ICRUDService< User, UserRequestDto, GenericResponseDTO, CustomUserResponseDTO > {
-    @Override
-    GenericResponseDTO create( UserRequestDto requestDto );
-
-    @Override
-    PageDataResponseDTO readAll(int pageNumber, int pageSize);
-
-    @Override
-    CustomUserResponseDTO readOne( Long id );
 }
