3751cece782ed14d8a8a8380d0e25f884d3033a4
MRParag95 <rahman.parag.95@gmail.com>
2024-07-13 10:32:42 +0600

    Initial Project Setup.

--- src/main/java/com/mendax47/learnspringboot/module/user/controller/UserController.java
+++ src/main/java/com/mendax47/learnspringboot/module/user/controller/UserController.java
@@ -3,7 +3,6 @@ package com.mendax47.learnspringboot.module.user.controller;
 import com.mendax47.learnspringboot.constants.routers.UserRoutes;
 import com.mendax47.learnspringboot.generics.dtos.responses.GenericResponseDTO;
 import com.mendax47.learnspringboot.module.user.dtos.requests.UserRequestDto;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
