e09efa119a9600629e4a4e97d1ef883c06d66471
MRParag95 <rahman.parag.95@gmail.com>
2024-07-14 12:35:30 +0600

    Initial Project Setup.

---
+++ src/main/java/com/mendax47/learnspringboot/config/security/ProjectSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.mendax47.learnspringboot.config.security;
+
+import com.mendax47.learnspringboot.constants.routers.PermittedRoutes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain( HttpSecurity http) throws Exception {
+        http
+                .cors( AbstractHttpConfigurer::disable )
+                .csrf( AbstractHttpConfigurer::disable )
+                .authorizeHttpRequests(( httpRequests ) -> {
+                    httpRequests
+                            .requestMatchers( PermittedRoutes.ROUTES )
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated();
+                })
+                .formLogin( Customizer.withDefaults() )
+                .httpBasic( Customizer.withDefaults() );
+
+        return http.build();
+    }
+}
