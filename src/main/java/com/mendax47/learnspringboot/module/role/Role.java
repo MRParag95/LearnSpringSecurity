a1194025bed73bbc7ffabe698f22c33fcb4d3f2c
MRParag95 <rahman.parag.95@gmail.com>
2024-07-14 11:53:48 +0600

    Initial Project Setup.

--- src/main/java/com/mendax47/learnspringboot/module/role/Role.java
+++ src/main/java/com/mendax47/learnspringboot/module/role/Role.java
@@ -31,7 +31,7 @@ import java.util.Set;
 @NoArgsConstructor
 @SuperBuilder
 @SQLRestriction( "is_active = TRUE" )
-@SQLDelete( sql = "UPDATE _user SET is_active = TRUE WHERE id = ?" )
+@SQLDelete( sql = "UPDATE _user SET is_active = FALSE WHERE id = ?" )
 @Entity
 public class Role extends BaseEntity {
     @Id
