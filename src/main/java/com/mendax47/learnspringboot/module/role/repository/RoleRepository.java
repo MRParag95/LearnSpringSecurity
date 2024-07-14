5f339fe483688d62b78801288b6b38ac4ce7f0ee
MRParag95 <rahman.parag.95@gmail.com>
2024-07-14 11:25:38 +0600

    Initial Project Setup.

--- src/main/java/com/mendax47/learnspringboot/module/role/repository/RoleRepository.java
+++ src/main/java/com/mendax47/learnspringboot/module/role/repository/RoleRepository.java
@@ -10,8 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Set;
-
 public interface RoleRepository extends JpaRepository< Role, Long >, JpaSpecificationExecutor< Role > {
     Role findByRoleType( RoleType roleType );
 
